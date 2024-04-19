221a8ed6c6c2bb19e8a5844bbeb3825d4fe0c1c3
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-04-19 19:21:58 +0200

    fixed cors

--- mhw3.js
+++ mhw3.js
@@ -105,7 +105,6 @@ function showmore(event) {
 ///////////////////////////////////////////////////////////////////////////////////////
 
 let token;
-let check;
 let game_name;
 let song_name;
 let movie_name;
@@ -113,16 +112,6 @@ let first_request = true;
 
 function onTokenJson(json) {
   token = json.access_token;
-}
-
-function show_movie_info() {}
-
-function show_music_info() {
-  console.log("prova musica");
-}
-
-function show_game(json) {
-  console.log(json);
 }
 
 function onResponse(response) {
@@ -134,8 +123,17 @@ function onResponse(response) {
 
 function onErrorCors(error) {
   console.clear();
-  first_request = false;
-  return true;
+  return null;
+}
+
+function show_movie_info() {}
+
+function show_music_info() {
+  console.log("prova musica");
+}
+
+function show_game(json) {
+  console.log(json);
 }
 
 async function show_game_info() {
@@ -161,17 +159,19 @@ async function show_game_info() {
     body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;",
   };
   if (first_request) {
-    check = await fetch(cors_proxy + target_url, options).then(onErrorCors);
+    const check = await fetch(cors_proxy + target_url, options).then(
+      onErrorCors
+    );
+    first_request = false;
+    show_game_info();
   } else {
     fetch(cors_proxy + target_url, options)
       .then(onResponse)
       .then(show_game);
   }
-  if (check) {
-    check = false;
-    show_game_info();
-  }
 }
+
+////////////////
 
 function onJson_Imdb(json) {
   if (!json) return null;
