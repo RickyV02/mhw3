62197bbb0780287749e4e4271ff4b76e3640ca78
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-04-19 18:33:18 +0200

    fixing cors

--- mhw3.js
+++ mhw3.js
@@ -105,7 +105,7 @@ function showmore(event) {
 ///////////////////////////////////////////////////////////////////////////////////////
 
 let token;
-let res;
+let check;
 let game_name;
 let song_name;
 let movie_name;
@@ -134,7 +134,7 @@ function onResponse(response) {
 }
 
 function onErrorCors(error) {
-  //console.clear();
+  console.clear();
   first_request = false;
   return true;
 }
@@ -160,15 +160,15 @@ async function show_game_info() {
     body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;",
   };
   if (first_request) {
-    res = await fetch(cors_proxy + target_url, options).then(onErrorCors);
+    check = await fetch(cors_proxy + target_url, options).then(onErrorCors);
   } else {
     fetch(cors_proxy + target_url, options)
       .then(onResponse)
       .then(show_game);
   }
-  if (res) {
+  if (check) {
+    check = false;
     show_game_info();
-    res = false;
   }
 }
 
